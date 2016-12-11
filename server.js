98a166b8f4d06077d2d39c802cfaa97f42aff0cc
Pitsanu0105 <jeng.39@hotmail.com>
2016-12-11 11:04:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/webhook/', function (req, res) {
      }, function(error, response, body) {
        try {
          var condition = body.main;
-         sendTextMessage(sender, "Today is " + condition.temp + "Celsius in " + location);
+         sendTextMessage(sender, "Today in " + location + " is " + condition.temp + "Celsius" + weather.description);
        } catch(err) {
          console.error('error caught', err);
          sendTextMessage(sender, "There was an error.");
