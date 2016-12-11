48195e40e2b8c05cc15a7d7317d6f3127f29f09a
Pitsanu0105 <jeng.39@hotmail.com>
2016-12-11 10:11:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.get('/webhook/', function (req, res) {
 })
 app.post('/webhook/', function (req, res) {
   let messaging_events = req.body.entry[0].messaging
-
   for (let i = 0; i < messaging_events.length; i++) {
     let event = req.body.entry[0].messaging[i]
     let sender = event.sender.id
@@ -29,7 +28,7 @@ app.post('/webhook/', function (req, res) {
         sendGenericMessage(sender)
         continue
       }
-      sendTextMessage(sender,'สวัสดีครับ วันนี้อยากทราบสภาพอากาศที่ไหนเอ่ย')
+      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
